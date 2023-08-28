2fef4d88bfec1f09eaf1036cf8cccf090bfd2c65
Anna <61278319+Anna18068391@users.noreply.github.com>
2023-08-28 20:32:11 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function show(){
+	document.getElementById("arttab").style.display ="block";
+}
+
+function hide(){
+	document.getElementById("arttab").style.display ="none";
+}
+
+function link(){
+	window.open('https://www.linkedin.com/in/anna-sivera-van-der-sluijs/');
+}
+
+
+window.onload=function(){
+
+	// const myNodeList = document.querySelectorAll("p");
+	// myNodeList[1]
+	
+	// document.querySelector("nav div").onclick=function(){
+	// 	show(this);
+	// }
+
+	document.getElementById("art").onclick=function(){
+		show(this);
+	}
+	document.getElementById("arttab").onclick=function(){
+		hide(this);
+	}
+	document.getElementById("link").onclick=function(){
+		link(this);
+	}
+}
